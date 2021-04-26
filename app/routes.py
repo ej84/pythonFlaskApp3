d36788b0d78111d1c6dbe98d8aaff480f488f0e0
ej84 <ej84@njit.edu>
2021-04-26 14:06:05 -0400

    part 1 and 2 completed.

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import current_app as app, Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Landing page."""
+    nav = [
+        {'name': 'Home', 'url': 'https://example.com/1'},
+        {'name': 'About', 'url': 'https://example.com/2'},
+        {'name': 'Pics', 'url': 'https://example.com/3'}
+    ]
+    return render_template(
+        'home.html',
+        nav=nav,
+        title="Jinja Demo Site",
+        description="Smarter page templates with Flask & Jinja."
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
